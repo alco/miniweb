d29bb275cf82b5e073b984bf2f9781374683d4bc
Alexei Sholik <alcosholik@gmail.com>
2014-06-13 06:54:28 +0300

    Moduledoc is no longer needed

--- lib/mix/tasks/muweb.ex
+++ lib/mix/tasks/muweb.ex
@@ -27,9 +27,9 @@ commands = [
 
   [name: "proxy",
    help: """
-     Work as a proxy, transmiting all traffic between client and remote
-     server without alterations, but logging all requests and responses to
-     stdout.
+     Work as a tunnelling proxy, logging all communications. All traffic
+     between client and remote server is transmitted without alterations, but
+     all requests and responses are logged to stdout.
      """,
   ],
 
@@ -50,7 +50,7 @@ commands = [
 spec = Commando.new [
   prefix: "mix",
   name: "muweb",
-  version: "muweb Mix task version #{Mix.Project.config[:version]}",
+  version: "μWeb #{Mix.Project.config[:version]}",
 
   help: "Single task encapsulating a set of useful commands that utilise the μWeb server.",
 
@@ -59,159 +59,32 @@ spec = Commando.new [
 ]
 
 defmodule Mix.Tasks.Muweb do
-  @shortdoc "Run μWeb commands"
-
   @cmdspec spec
 
-  @moduledoc """
-  Synopsis:
-    mix muweb [OPTIONS] COMMAND [ARG...]
-
-  Single task encapsulating a set of useful commands
-  that utilise the μWeb server.
-
-  Options (available for all commands except "help"):
-    -h, --host
-        Hostname to resolve. Accepts extended format with port, e.g.
-        "localhost:4000".
-
-    -p, --port
-        Port number to listen on.
-
-  Commands:
-    help       Display help for a command
-    inspect    Log all incoming requests to stdout
-    proxy      Work as a tunnelling proxy, logging all communications
-    serve      Serve files from a given directory
-  """
+  @shortdoc "Run μWeb commands"
+  @moduledoc Commando.help(spec)
 
   use Mix.Task
 
   def run(args) do
-    IO.inspect args
     {:ok, %Commando.Cmd{options: opts, subcmd: cmd}} =
       Commando.parse(args, @cmdspec)
     run_cmd(cmd.name, cmd, opts)
   end
 
-  #def run([]), do: usage()
-
-  #def run(args) do
-    #case OptionParser.parse_head(args, aliases: [p: :port, h: :host]) do
-      #{_, _, [h|_]} ->
-        #IO.puts "Unrecognized option -#{h}"
-
-      #{_, [], []} ->
-        #usage()
-
-      #{opts, [cmd|rest], []} ->
-        #IO.puts "Applying opts #{inspect opts}"
-        #run_cmd(cmd, rest)
-    #end
-  #end
-
-
-  #def run_cmd("help", []),    do: usage()
-  #def run_cmd("help", [cmd]), do: usage(cmd)
-
-
   def run_cmd("inspect", _cmd, _options) do
-    # mix muweb [OPTIONS] inspect [-f PATH|--reply-file=PATH]
-    #case OptionParser.parse(args, aliases: [f: :reply_file]) do
-      #{_, _, [h|_]} ->
-        #IO.puts "Unrecognized option -#{h}"
-
-      #{_opts, [], []} ->
-        IO.puts "inspecting..."
-
-      #_ -> nil #usage("inspect")
-    #end
+    IO.puts "inspecting..."
+    #_ -> nil #usage("inspect")
   end
 
+  def run_cmd("proxy", _cmd, _options) do
+    IO.puts "proxying..."
+  end
 
   def run_cmd("serve", %Commando.Cmd{arguments: %{"path" => dir}}, options) do
     IO.puts "Serving directory: #{dir}"
     serve(dir, options[:port])
   end
-
-  #def run_cmd("serve", [path]) do
-    #IO.puts "Serving path #{path}..."
-    #serve(path)
-  #end
-
-
-  #def run_cmd(cmd, args), do: (IO.inspect args; usage(cmd))
-
-  ###
-
-  #defp usage do
-    #case __info__(:moduledoc) do
-      #{_, binary} when is_binary(binary) ->
-        #Mix.shell.info binary
-
-      #_ -> Mix.shell.info "#{inspect __MODULE__} was not compiled with docs"
-    #end
-  #end
-
-  #defp usage("help") do
-    #Mix.shell.info """
-    #Synopsis:
-      #mix muweb help [COMMAND]
-
-    #Display description of the given command.
-    #"""
-  #end
-
-  #defp usage("inspect") do
-    #Mix.shell.info """
-    #Synopsis:
-      #mix muweb [OPTIONS] inspect [-f PATH|--reply-file=PATH]
-
-    #Log incoming requests to stdout, optionally sending a reply back.
-
-    #Options:
-      #-f PATH, --reply-file=PATH
-         #Send the contents of file at PATH in reponse to incoming requests.
-
-         #By default, nothing is sent in response, the connection is closed
-         #immediately.
-
-         #Passing a dash (-) for PATH will read from stdin.
-    #"""
-  #end
-
-  #defp usage("proxy") do
-    #Mix.shell.info """
-    #Synopsis:
-      #mix muweb [OPTIONS] proxy
-
-    #Work as a proxy, transmiting all traffic between client and remote server
-    #without alterations, but logging all requests and responses to stdout.
-    #"""
-  #end
-
-  #defp usage("serve") do
-    #Mix.shell.info """
-    #Synopsis:
-      #mix muweb [OPTIONS] serve [-l|--list] [PATH]
-
-    #Serve files from the specified directory, recursively.
-
-    #Arguments:
-      #PATH
-          #The directory to serve (default: .).
-
-    #Options:
-      #-l, --list
-          #For directory requests, serve HTML with the list of contents of that
-          #directory.
-
-          #Without this option, "403 Forbidden" is returned for
-          #directory requests.
-    #"""
-  #end
-
-  #defp usage(_), do: usage
 
   ###
 
--- lib/muweb.ex
+++ lib/muweb.ex
@@ -3,8 +3,6 @@ defmodule MuWeb do
 
   use Application.Behaviour
 
-  # See http://elixir-lang.org/docs/stable/Application.Behaviour.html
-  # for more information on OTP Applications
   def start(_type, _args) do
     MuWeb.Supervisor.start_link
   end
